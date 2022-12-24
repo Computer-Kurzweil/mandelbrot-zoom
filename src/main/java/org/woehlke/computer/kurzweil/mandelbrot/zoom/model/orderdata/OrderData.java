0d00c106a70d8f98d848613ef9bce16c01c8e96a
thomaswoehlke <thomas.woehlke@gmail.com>
2022-12-24 02:51:37 +0100

    merge

--- src/main/java/org/woehlke/computer/kurzweil/mandelbrot/zoom/model/orderdata/OrderData.java
+++ src/main/java/org/woehlke/computer/kurzweil/mandelbrot/zoom/model/orderdata/OrderData.java
@@ -3,4 +3,10 @@ package org.woehlke.computer.kurzweil.mandelbrot.zoom.model.orderdata;
 import java.io.Serializable;
 
 public class OrderData implements Serializable {
+    // Click Coordinates lattice x,y
+    // Old  Rectangle lattice height, width
+    // Old Coordinates lattice x,yas complexnumber
+    // Clic Rectangle lattice height, width
+
+
 }
